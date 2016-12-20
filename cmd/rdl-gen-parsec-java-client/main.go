df06813a12e64c8465d2bd78d58a6119b897d309
guang001 <guang001@yahoo-inc.com>
2016-12-20 23:38:59 +0800

    init client generator

---
+++ cmd/rdl-gen-parsec-java-client/main.go
@@ -0,0 +1,495 @@
+// Copyright 2016 Yahoo Inc.
+// Licensed under the terms of the Apache license. Please see LICENSE.md file distributed with this work for terms.
+
+package main
+
+import (
+	"github.com/ardielle/ardielle-go/rdl"
+	"bufio"
+	"strings"
+	"log"
+	"flag"
+	"io/ioutil"
+	"fmt"
+	"encoding/json"
+	"os"
+	"github.com/yahoo/parsec-rdl-gen/utils"
+	"text/template"
+)
+
+type javaClientGenerator struct {
+	registry rdl.TypeRegistry
+	schema   *rdl.Schema
+	name     string
+	writer   *bufio.Writer
+	err      error
+	banner   string
+	ns       string
+	base     string
+}
+
+func main() {
+	pOutdir := flag.String("o", ".", "Output directory")
+	flag.String("s", "", "RDL source file")
+	namespace := flag.String("ns", "", "Namespace")
+	flag.Parse()
+	data, err := ioutil.ReadAll(os.Stdin)
+	banner := "parsec-rdl-gen (development version)"
+
+	if err == nil {
+		var schema rdl.Schema
+		err = json.Unmarshal(data, &schema)
+		if err == nil {
+			GenerateJavaClient(banner, &schema, *pOutdir, *namespace, "")
+			os.Exit(0)
+		}
+	}
+	fmt.Fprintf(os.Stderr, "*** %v\n", err)
+	os.Exit(1)
+}
+
+// GenerateJavaClient generates the client code to talk to the server
+func GenerateJavaClient(banner string, schema *rdl.Schema, outdir string, ns string, base string) error {
+
+	reg := rdl.NewTypeRegistry(schema)
+
+	packageSrcDir, err := utils.JavaGenerationSourceDir(schema, ns)
+	if err != nil {
+		return err
+	}
+
+	cName := utils.Capitalize(string(schema.Name))
+
+	_, filePath := utils.GetOutputPathInfo(packageSrcDir, cName, "ClientImpl.java")
+	if _, err := os.Stat(filePath); err == nil {
+		fmt.Fprintln(os.Stderr, "Warning: interface implementation class exists, ignore: ", filePath)
+	} else {
+		out, file, _, err := utils.OutputWriter(packageSrcDir, cName, "ClientImpl.java")
+		if err != nil {
+			return err
+		}
+		gen := &javaClientGenerator{reg, schema, cName, out, nil, banner, ns, base}
+		gen.processTemplate(javaClientTemplate)
+		out.Flush()
+		file.Close()
+		if gen.err != nil {
+			return gen.err
+		}
+	}
+
+	_, filePath = utils.GetOutputPathInfo(packageSrcDir, cName, "Client.java")
+	if _, err := os.Stat(filePath); err == nil {
+		fmt.Fprintln(os.Stderr, "Warning: interface class exists, ignore: ", filePath)
+	} else {
+		out, file, _, err := utils.OutputWriter(packageSrcDir, cName, "Client.java")
+		if err != nil {
+			return err
+		}
+		gen := &javaClientGenerator{reg, schema, cName, out, nil, banner, ns, base}
+		gen.processTemplate(javaClientInterfaceTemplate)
+		out.Flush()
+		file.Close()
+		if gen.err != nil {
+			return gen.err
+		}
+	}
+
+	packageDir, err := utils.JavaGenerationDir(outdir, schema, ns)
+	out, file, _, err := utils.OutputWriter(packageDir, "ParsecResponseFuture", ".java")
+	if err != nil {
+		return err
+	}
+	gen := &javaClientGenerator{reg, schema, cName, out, nil, banner, ns, base}
+	gen.processTemplate(javaClientResponseFutureTemplate)
+	out.Flush()
+	file.Close()
+	if gen.err != nil {
+		return gen.err
+	}
+
+	//ResourceException - the throawable wrapper for alternate return types
+	out, file, _, err = utils.OutputWriter(packageDir, "ResourceException", ".java")
+	if err != nil {
+		return err
+	}
+	err = utils.JavaGenerateResourceException(schema, out, ns)
+	out.Flush()
+	file.Close()
+	if err != nil {
+		return err
+	}
+
+	//ResourceError - the default data object for an error
+	out, file, _, err = utils.OutputWriter(packageDir, "ResourceError", ".java")
+	if err != nil {
+		return err
+	}
+	err = utils.JavaGenerateResourceError(schema, out, ns)
+	out.Flush()
+	file.Close()
+	return err
+}
+
+func (gen *javaClientGenerator) processTemplate(templateSource string) error {
+	commentFun := func(s string) string {
+		return utils.FormatComment(s, 0, 80)
+	}
+	funcMap := template.FuncMap{
+		"header":      func() string { return utils.JavaGenerationHeader(gen.banner) },
+		"package":     func() string { return utils.JavaGenerationPackage(gen.schema, gen.ns) },
+		"comment":     commentFun,
+		"methodSig":   func(r *rdl.Resource) string { return "public "+ gen.clientMethodSignature(r) },
+		"name":        func() string { return gen.name },
+		"cName":       func() string { return utils.Capitalize(gen.name) },
+		"lName":       func() string { return utils.Uncapitalize(gen.name) },
+		"needBody":    func(r *rdl.Resource) bool { return gen.needBody(r) },
+		"bodyObj":     func(r *rdl.Resource) string { return gen.getBodyObj(r) },
+		"iMethod":     func(r *rdl.Resource) string { return gen.clientMethodSignature(r) + ";" },
+		"builderExt":  func(r *rdl.Resource) string { return gen.builderExt(r) },
+		"origPackage":  func() string { return utils.JavaGenerationOrigPackage(gen.schema, gen.ns) },
+		"origHeader":   func() string { return utils.JavaGenerationOrigHeader(gen.banner) },
+	}
+	t := template.Must(template.New(gen.name).Funcs(funcMap).Parse(templateSource))
+	return t.Execute(gen.writer, gen.schema)
+}
+
+func (gen* javaClientGenerator) builderExt(r *rdl.Resource) string {
+	code := ""
+	for _, input := range r.Inputs {
+		iname := javaName(input.Name)
+		if input.PathParam {
+			code += ".resolveTemplate(\"" + iname + "\", " + iname + ")\n"
+		} else if input.QueryParam != "" {
+			code += ".queryParam(\"" + iname + "\", " + iname + ")\n"
+		}
+	}
+	code += "                            .build();"
+	return code
+}
+
+func (gen* javaClientGenerator) getBodyObj(r *rdl.Resource) string {
+	idx, ok := gen.findFirstUserDefType(r.Inputs)
+	if ok { return javaName(r.Inputs[idx].Name) }
+	return ""
+}
+
+func (gen* javaClientGenerator) findFirstUserDefType(resInputs []*rdl.ResourceInput) (int, bool) {
+	for idx, input := range resInputs {
+		userType := gen.registry.FindBaseType(input.Type)
+		// todo: need consider map or array case
+		if userType == rdl.BaseTypeStruct {
+			return idx, true
+		}
+	}
+	return -1, false
+}
+
+func (gen *javaClientGenerator) needBody(r *rdl.Resource) bool {
+	// check inputs is user defined type or not
+	_, ok := gen.findFirstUserDefType(r.Inputs)
+	return ok
+}
+
+const javaClientResponseFutureTemplate = `{{header}}
+package {{package}};
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.ws.rs.core.Response;
+import javax.xml.ws.http.HTTPException;
+import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class ParsecResponseFuture<T> implements Future {
+    private final Future<Response> future;
+    private final Class<T> tClass;
+    private final ObjectMapper objectMapper;
+    private final Set<Integer> expectedStatusCodes;
+
+    public ParsecResponseFuture(Future<Response> future, Class<T> tClass,
+                                Set<Integer> expectedStatusCodes, ObjectMapper objectMapper) {
+        this.future = future;
+        this.tClass = tClass;
+        this.objectMapper = objectMapper;
+        this.expectedStatusCodes = expectedStatusCodes;
+    }
+
+
+    public ParsecResponseFuture(Future<Response> future, Class<T> tClass,
+                                Set<Integer> expectedStatusCodes) {
+        this(future, tClass, expectedStatusCodes, new ObjectMapper());
+    }
+
+    /**
+     * Get the response.
+     * @return Response response
+     * @throws ExecutionException Execution exception
+     * @throws InterruptedException Interrupted exception
+     */
+    public Response getResponse() throws ExecutionException, InterruptedException {
+        return future.get();
+    }
+
+    @Override
+    public boolean cancel(boolean mayInterruptIfRunning) {
+        return future.cancel(mayInterruptIfRunning);
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return future.isCancelled();
+    }
+
+    @Override
+    public boolean isDone() {
+        return future.isDone();
+    }
+
+    /**
+     * Get the response object.
+     * @return T
+     * @throws ExecutionException Execution exception.
+     * @throws InterruptedException Interrupted exception.
+     */
+    @Override
+    public T get() throws ExecutionException, InterruptedException, HTTPException {
+        Response response = getResponse();
+        int statusCode = response.getStatus();
+
+        if (expectedStatusCodes != null && expectedStatusCodes.contains(statusCode)) {
+            try {
+                if (response.hasEntity()) {
+                    String body = response.getEntity().toString();
+                    return objectMapper.readValue(body, tClass);
+                }
+                return null;
+            } catch (IOException e) {
+                throw new RuntimeException("Invalid Response Body");
+            }
+        } else {
+            throw new HTTPException(statusCode);
+        }
+    }
+
+    @Override
+    public T get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException,
+            TimeoutException, HTTPException {
+        // make sure that we can get the response before timeout
+        future.get(timeout, unit);
+        return get();
+    }
+}
+`
+const javaClientInterfaceTemplate = `{{origHeader}}
+package {{origPackage}}
+
+import {{package}}.ResourceException;
+{{range .Types}}{{if .StructTypeDef}}{{if .StructTypeDef.Name}}import {{package}}.{{.StructTypeDef.Name}};
+{{end}}{{end}}{{end}}
+
+public interface {{cName}}Client {
+{{range .Resources}}
+    {{iMethod .}}{{end}}
+}
+`
+const javaClientTemplate = `{{origHeader}}
+package {{origPackage}};
+
+import {{package}}.ResourceException;
+{{range .Types}}{{if .StructTypeDef}}{{if .StructTypeDef.Name}}import {{package}}.{{.StructTypeDef.Name}};
+{{end}}{{end}}{{end}}
+import com.yahoo.ec.parsec.clients.ParsecAsyncHttpClient;
+import com.yahoo.ec.parsec.clients.ParsecAsyncHttpRequest;
+import com.yahoo.ec.parsec.clients.ParsecAsyncHttpRequest.Builder;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class {{cName}}ClientImpl implements {{cName}}Client {
+
+    /** Logger. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(SampleClientImpl.class);
+
+    /** ParsecAsyncHttpClient. */
+    private final ParsecAsyncHttpClient parsecAsyncHttpClient;
+
+    /** Object mapper */
+    private final ObjectMapper objectMapper;
+
+    /** URL. */
+    private String url;
+
+    /** Headers. */
+    private final Map<String, String> headers;
+
+    /**
+     * connection timeout.
+     */
+    private static final int IDLE_CONNECTION_TIMEOUT_IN_MS = 15000;
+
+    /**
+     * total connections.
+     */
+    private static final int MAXIMUM_CONNECTIONS_TOTAL = 50;
+
+    public {{cName}}ClientImpl(
+        String url,
+        Map<String, String> headers
+    ) {
+
+        ParsecAsyncHttpClient client  = null;
+        try {
+            client = new ParsecAsyncHttpClient.Builder()
+                .setAcceptAnyCertificate(true)
+                .setAllowPoolingConnections(true)
+                .setPooledConnectionIdleTimeout(IDLE_CONNECTION_TIMEOUT_IN_MS)
+                .setMaxConnections(MAXIMUM_CONNECTIONS_TOTAL)
+                .build();
+        } catch (ExecutionException e) {
+            LOGGER.error("create ParsecAsyncHttpClient failed. " + e.getMessage());
+            throw new ResourceException(ResourceException.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+        this.parsecAsyncHttpClient = client;
+        this.objectMapper = new ObjectMapper();
+        this.url = url;
+        this.headers = headers;
+    }
+
+    public {{cName}}ClientImpl (
+            ParsecAsyncHttpClient client,
+            ObjectMapper objectMapper,
+            String url,
+            Map<String, String> headers)
+    {
+        this.parsecAsyncHttpClient = client;
+        this.objectMapper = objectMapper;
+        this.url = url;
+        this.headers = headers;
+    }
+
+    private ParsecAsyncHttpRequest getRequest(String method, URI uri, String body) throws ResourceException {
+        Builder builder = new Builder();
+
+        builder.setUri(uri);
+        if (headers != null) {
+            for (Map.Entry<String, String> entry : headers.entrySet()) {
+                builder.addHeader(entry.getKey(), entry.getValue());
+            }
+        }
+
+        builder.setMethod(method);
+
+        builder.setBody(body).setBodyEncoding("UTF-8");
+
+        ParsecAsyncHttpRequest request = null;
+        try {
+            request = builder.build();
+        } catch (Exception e) {
+            LOGGER.error("builder build failed: " + e.getMessage());
+            throw new ResourceException(ResourceException.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+        return request;
+    }
+{{range .Resources}}
+    @Override
+    {{methodSig .}} {
+        String path = "{{.Path}}";
+        String body = null;
+        {{if needBody .}}
+        try {
+            body = objectMapper.writeValueAsString({{bodyObj .}});
+        } catch (JsonProcessingException e) {
+            LOGGER.error("JsonProcessingException: " + e.getMessage());
+            throw new ResourceException(ResourceException.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+        {{end}}
+        URI uri = UriBuilder.fromUri(url).path(path)
+                            {{builderExt .}}
+        ParsecAsyncHttpRequest request = getRequest("{{.Method}}", uri, body);
+
+        CompletableFuture<Response> response = null;
+        try {
+            response = parsecAsyncHttpClient.execute(request);
+        } catch (ExecutionException e) {
+            LOGGER.error("ExecutionException: " + e.getMessage());
+            throw new ResourceException(ResourceException.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+
+        Set<Integer> expectedStatus = new HashSet<>();
+        expectedStatus.add(ResourceException.{{.Expected}});
+        {{if .Alternatives}}{{range .Alternatives}}expectedStatus.add(ResourceException.{{.}});{{end}}{{end}}
+        return new ParsecResponseFuture<>(response, User.class, expectedStatus, objectMapper);
+    }
+{{end}}
+}
+`
+
+// todo: copy from go-schema.go
+func safeTypeVarName(rtype rdl.TypeRef) rdl.TypeName {
+	tokens := strings.Split(string(rtype), ".")
+	return rdl.TypeName(utils.Capitalize(strings.Join(tokens, "")))
+}
+
+// todo: duplicate with server code, need integrate
+func javaMethodName(reg rdl.TypeRegistry, r *rdl.Resource) (string, []string) {
+	var params []string
+	bodyType := string(safeTypeVarName(r.Type))
+	for _, v := range r.Inputs {
+		if v.Context != "" { //ignore these legacy things
+			log.Println("Warning: v1 style context param ignored:", v.Name, v.Context)
+			continue
+		}
+		k := v.Name
+		if v.QueryParam == "" && !v.PathParam && v.Header == "" {
+			bodyType = string(safeTypeVarName(v.Type))
+		}
+		optional := false // but different with server code, how?
+		params = append(params, utils.JavaType(reg, v.Type, optional, "", "")+" "+javaName(k))
+	}
+	return strings.ToLower(string(r.Method)) + string(bodyType), params
+}
+
+// todo: duplicate with java-server.go
+func javaName(name rdl.Identifier) string {
+	switch name {
+	case "type", "default": //other reserved words
+		return "_" + string(name)
+	default:
+		return string(name)
+	}
+}
+
+func (gen *javaClientGenerator) clientMethodSignature(r *rdl.Resource) string {
+	reg := gen.registry
+	returnType := utils.JavaType(reg, r.Type, true, "", "")
+	methName, params := javaMethodName(reg, r)
+	sparams := ""
+	if len(params) > 0 {
+		sparams = strings.Join(params, ", ")
+	}
+	if len(r.Outputs) > 0 {
+		if sparams == "" {
+			sparams = "java.util.Map<String,java.util.List<String>> headers"
+		} else {
+			sparams = sparams + ", java.util.Map<String,java.util.List<String>> headers"
+		}
+	}
+	return "ParsecResponseFuture<" + returnType + "> " + methName + "(" + sparams + ") throws ResourceException"
+}
+
+
