6f42e4b517247478db1722500e42d5b5c69bf874
Joe Hsu <joe321@yahoo-inc.com>
2017-01-06 16:31:19 +0800

    remove unused import

--- cmd/rdl-gen-parsec-java-client/main.go
+++ cmd/rdl-gen-parsec-java-client/main.go
@@ -218,12 +218,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
-import java.util.HashSet;
+{{if needExpect .}}import java.util.HashSet;
+import java.util.Set;{{end}}
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
